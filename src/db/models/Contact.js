b7bf97609fdb274dd295acb632f2a6f2d03234a0
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-04-17 11:17:04 +0200

    Fix POST

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,29 +1,35 @@
 import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  email: {
-    type: String,
-  },
-  isFavourite: {
-    type: Boolean,
-    default: false,
-    required: true,
-  },
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    required: true,
-    default: 'personal',
-  },
-});
+);
 
 const ContactCollection = model('contact', contactSchema);
 
